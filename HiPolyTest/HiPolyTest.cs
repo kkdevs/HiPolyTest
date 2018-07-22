e0ac311b90ccf8c86134657ba31533ce53d7854c
x <x@x>
2018-07-22 02:28:52 +0200

    import

---
+++ HiPolyTest/HiPolyTest.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using Harmony;
+using System.Reflection;
+using System;
+using UnityEngine;
+
+public class HiPolyTest : BaseUnityPlugin
+{
+	[HarmonyPrefix, HarmonyPatch(typeof(ChaControl), "InitBaseCustomTextureBody")]
+	public static bool SetHiPolyFlag(ChaControl __instance)
+	{
+		typeof(ChaControl).GetProperty("hiPoly", BindingFlags.Instance|BindingFlags.NonPublic| BindingFlags.Public| BindingFlags.FlattenHierarchy).SetValue(__instance, true, null);
+		return true;
+	}
+
+	public void Awake() => HarmonyInstance.Create("HiPolyTest").PatchAll(typeof(HiPolyTest));
+}
